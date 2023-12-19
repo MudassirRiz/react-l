fb2de17231f1a78793e8b39eaaffac5dbda6a221
MudassirRiz <mdssrrizvi@gmail.com>
2023-12-19 13:29:51 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Sam from "./sam"
+
+function App() {
+
+  return (
+   <Sam/>
+  
+  )
+}
+
+export default App
---
+++ src/sam.jsx
@@ -0,0 +1,7 @@
+function Sam(){
+    return(
+        <h3>pagal h hm sab</h3>
+    )
+}
+
+export default Sam 
